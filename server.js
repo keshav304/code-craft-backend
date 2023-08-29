55c84c2d5a28a390eec653622fda78c2d12e857c
Keshav Jha <keshav@personifyxp.com>
2023-08-29 14:22:30 +0530

    setup

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import  bodyParser from 'body-parser';
+import  mongoose from 'mongoose';
+import  morgan from 'morgan'
+import  cors from 'cors';
+import  dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+app.use(cors());
+
+
+//morgan used for logging
+app.use(morgan('dev'));
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+
+
+
+
+
+// connecting to database
+const CONNECTION_URL = process.env.CONNECTION_URL
+const PORT = process.env.PORT|| 8000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on PORT: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
